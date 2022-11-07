588404114b14090066b89af17c67fdc38f05ede9
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-11-07 17:36:00 -0300

    feat: update modals js/css

---
+++ src/scripts/modals.js
@@ -0,0 +1,48 @@
+/* --------------- CRIA CONTAINER PADRÃO PARA OS MODAIS -------------- */
+const createModalContainer = () => {
+    const body = document.querySelector('body')
+
+    const divContainer = document.createElement('div')
+    const divModal = document.createElement('div')
+    const divHeader = document.createElement('div')
+    const button = document.createElement('button')
+    const img = document.createElement('img')
+    const divContent = document.createElement('div')
+    const divFooter = document.createElement('div')
+
+    divContainer.className = 'modal-container'
+    divModal.className = 'modal'
+    divHeader.className = 'header-modal'
+    button.id = 'close-modal'
+    img.src = '../images/close.svg'
+    divContent.className = 'content-modal'
+    divFooter.classList = 'footer-modal'
+
+    divContainer.appendChild(divModal)
+    divModal.append(divHeader, divContent, divFooter)
+    divHeader.appendChild(button)
+    button.appendChild(img)
+
+    body.appendChild(divContainer)
+
+    button.onclick = () => divContainer.remove()
+
+    return divContent
+}
+
+
+/* --------------- CRIA MODAL DE ATUALIZAÇÃO DE PERFIL -------------- */
+export const createModalUpdateProfile = () => {
+    const modalContainer = createModalContainer()
+
+    modalContainer.insertAdjacentHTML('beforeend',
+        `<h2>Atualizar perfil</h2>
+        <form>
+            <input type="text" placeholder="Nome">
+            <input type="text" placeholder="Email">
+            <input type="text" placeholder="Avatar">
+            <button class='btn btn-primary'>Atualizar</button>
+        </form>
+        `
+    )
+}
