f08304cd3f94e4b9360a816e64876d25b6939df5
Rhuankb <rhuankb@gmail.com>
2022-11-08 16:13:18 -0300

    feat:adiciona informações dos pets de forma dinâmica

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,4 +1,61 @@
-import { getUserProfile } from "./requests.js"
+import { getPetsUser, getUserProfile } from "./requests.js"
+
+const insertPets = async () => {
+    const pets = await getPetsUser("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njc5MjIwNzcsImV4cCI6MTY2ODUyNjg3Nywic3ViIjoiN2EyNDRjNmQtZGQyOS00YzI2LThkOGYtZGEzZGI1NDUzY2U4In0.XBdE5HR2bicnsKPDl-4DqaCAyCpNLvdHjs_lqxnhy0E")
+    pets.forEach(element => {
+        const ul = document.querySelector("ul")
+        const li = document.createElement("li")
+        const img = document.createElement("img")
+        const div = document.createElement("div")
+        const nome = document.createElement("p")
+        const spanNome = document.createElement("span")
+        const spanNomeReal = document.createElement("span")
+        const especie = document.createElement("p")
+        const spanEspecie = document.createElement("span")
+        const spanEspecieReal = document.createElement("span")
+        const adopt = document.createElement("p")
+        const spanAdopt = document.createElement("span")
+        const spanAdoptReal = document.createElement("span")
+        const button = document.createElement("button")
+
+        li.classList = "display-flex"
+        img.classList = "card-image"
+        img.src = `${element.avatar_url}`
+        div.classList = "display-flex info-pet flex-direction-column justify-evenly"
+        spanNome.classList = "font-body-brand"
+        spanNome.innerText = 'Nome:'
+        spanNomeReal.classList = "font-body-brand"
+        spanNomeReal.innerText = `${element.name}`
+        spanEspecie.classList = "font-body-brand"
+        spanEspecie.innerText = 'Espécie:'
+        spanEspecieReal.classList = "font-body-brand"
+        spanEspecieReal.innerText = `${element.species}`
+
+        spanAdopt.classList = "font-body-brand"
+        spanAdopt.innerText = 'Adotável:'
+        spanAdoptReal.classList = "font-body-brand"
+        if (element.available_for_adoption == true) {
+            spanAdoptReal.innerText = "Sim"
+        } else {
+            spanAdoptReal.innerText = "Sim"
+        }
+
+        button.classList = "btn btn-primary att-pet"
+        button.id = `${element.id}`
+        button.innerText = "Atualizar"
+
+        nome.append(spanNome, spanNomeReal)
+        especie.append(spanEspecie, spanEspecieReal)
+        adopt.append(spanAdopt, spanAdoptReal)
+
+        div.append(nome, especie, adopt, button)
+
+        li.append(img, div)
+
+        ul.appendChild(li)
+    });
+
+}
 
 const dinamicPage = async () => {
     const user = await getUserProfile("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njc5MjIwNzcsImV4cCI6MTY2ODUyNjg3Nywic3ViIjoiN2EyNDRjNmQtZGQyOS00YzI2LThkOGYtZGEzZGI1NDUzY2U4In0.XBdE5HR2bicnsKPDl-4DqaCAyCpNLvdHjs_lqxnhy0E")
@@ -22,7 +79,23 @@ const dinamicPage = async () => {
     <div>
         <button class="register-pet btn btn-green" type="button">Cadastrar novo pet</button>
     </div>
+    <ul class="display-flex wrap">
+    </ul>
 `)
+
+insertPets()
 }
 
 dinamicPage()
+
+{/* <li class="display-flex">
+<img class="card-image"
+    src="https://d3ugyf2ht6aenh.cloudfront.net/stores/001/660/386/themes/style/img-1528169277-1626147375-477d36b93f28cbb72b20e24d3fb163591626147376.png?486223581">
+<div class="display-flex info-pet flex-direction-column justify-evenly">
+    <p><span class="font-body-brand">Nome:</span> <span class="font-body-black">Bidu</span></p>
+    <p><span class="font-body-brand">Espécie:</span> <span class="font-body-black">Cachorro</span>
+    </p>
+    <p><span class="font-body-brand">Adotável:</span> <span class="font-body-black">Sim</span></p>
+    <button class="btn btn-primary att-pet">Atualizar</button>
+</div>
+</li> */}
