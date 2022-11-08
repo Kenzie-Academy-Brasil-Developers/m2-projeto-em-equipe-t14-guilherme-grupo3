478f3d2a780ce9e4b509455c2c839f115efbf637
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-11-08 19:28:42 -0300

    fix: centered the cards on the home page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import { renderCardsHome } from "./render.js";
 import { getAllPets } from "./requests.js";
-import { createModalRegister } from "./modals.js";
 import { registerUser } from "./register.js";
 
 const petsProfile = await getAllPets()
