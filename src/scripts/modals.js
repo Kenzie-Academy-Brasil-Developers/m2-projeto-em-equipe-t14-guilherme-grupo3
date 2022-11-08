d87eb573bab8df3db98f9b2671aea5e35ff5fa3e
Rhuankb <rhuankb@gmail.com>
2022-11-08 15:20:34 -0300

    fix:Cria nova função de modal

--- src/scripts/modals.js
+++ src/scripts/modals.js
@@ -1,5 +1,5 @@
 /* --------------- CRIA CONTAINER PADRÃO PARA OS MODAIS -------------- */
-const createModalContainer = (form) => {
+export const createModalContainer = (form) => {
     const body = document.querySelector('body')
 
     const divContainer = document.createElement('div')
@@ -30,6 +30,38 @@ const createModalContainer = (form) => {
     
 }
 
+const createModalContainerRegister = () => {
+    const body = document.querySelector('body')
+
+    const divContainer = document.createElement('div')
+    const divModal = document.createElement('div')
+    const divHeader = document.createElement('div')
+    const button = document.createElement('button')
+    const img = document.createElement('img')
+    const divContent = document.createElement('div')
+    const divFooter = document.createElement('div')
+
+    divContainer.className = 'modal-container'
+    divModal.className = 'modal'
+    divHeader.className = 'header-modal'
+    button.id = 'close-modal'
+    img.src = '../images/close.svg'
+    divContent.className = 'content-modal'
+    divFooter.classList = 'footer-modal'
+
+    divContainer.appendChild(divModal)
+    divModal.append(divHeader, divContent, divFooter)
+    divHeader.appendChild(button)
+    button.appendChild(img)
+
+    body.appendChild(divContainer)
+
+    button.onclick = () => divContainer.remove()
+
+    return divContent
+    
+}
+
 
 /* --------------- CRIA MODAL DE ATUALIZAÇÃO DE PERFIL -------------- */
 export const createModalUpdateProfile = () => {
@@ -50,7 +82,7 @@ export const createModalUpdateProfile = () => {
 
 // ------------------CRIA MODAL DE REGISTRO--------------------------
 export const createModalRegister = () => {
-    const modalContainer = createModalContainer()
+    const modalContainer = createModalContainerRegister()
     const button = document.querySelector("#close-modal")
     const img = button.firstChild
     img.src = "./src/images/close.svg"
@@ -59,11 +91,12 @@ export const createModalRegister = () => {
         `<h2 class='font-brand'>Cadastrar</h2>
         <form>
             <input type="text" id="name" placeholder="Nome">
-            <input type="text" id="email" placeholder="Email">
-            <input type="text" id="password" placeholder="Senha">
+            <input type="email" id="email" placeholder="Email">
+            <input type="password" id="password" placeholder="Senha">
             <input type="text" id="avatar_url" placeholder="Avatar">
             <button type="submit" class='btn btn-primary'>Cadastrar</button>
             <p class="font-gray">Já tem cadastro? <a class="redirect-login">Clique aqui</a> para logar.</p>
         </form>
       `
     )
+}
--- src/scripts/register.js
+++ src/scripts/register.js
@@ -29,7 +29,6 @@ export const registerUser = () => {
                 }
             })
             await createUser(body)
-
         })
 
     })   
